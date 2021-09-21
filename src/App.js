da3b22d8661343092273f8e046e85cc3eec34e81
BsgMax00 <r0830999@student.thomasmore.be>
2021-09-21 11:03:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <h1>Menu</h1>
+        <div>Cola</div>
+        <div>Water</div>
+        <div>Bier</div>
+        <div>Wijn</div>
+      </div>
+  );
+}
+
+export default App;

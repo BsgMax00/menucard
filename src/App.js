09e245262ca2d97f62de0642ae9e75525517019b
BsgMax00 <r0830999@student.thomasmore.be>
2021-09-21 11:34:08 +0200

    klaar met 4 (kreeg ze niet mooi naast elkaar)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+function ProductLine(props){
+    const{productName} = props;
+    const{productCost} = props;
+    return <div>{productName} {productCost}</div>
+}
+
 function App() {
   return (
       <div>
         <h1>Menu</h1>
-        <div>Cola</div>
-        <div>Water</div>
-        <div>Bier</div>
-        <div>Wijn</div>
+        <ProductLine productName = "Cola" productCost = "1€"/>
+        <ProductLine productName = "Water" productCost = "1€"/>
+        <ProductLine productName = "Bier" productCost = "1€"/>
+        <ProductLine productName = "Wijn" productCost = "2€"/>
       </div>
   );
 }

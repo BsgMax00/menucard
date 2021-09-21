d7a9a7060ade7ad511a0082d85ea421ced2d8706
BsgMax00 <r0830999@student.thomasmore.be>
2021-09-21 11:42:46 +0200

    klaar met 5.1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {PRODUCTS_DATA} from "./data/data";
 
 function ProductLine(props){
-    const{productName} = props;
-    const{productCost} = props;
-    return <div>{productName} {productCost}</div>
+    const{product} = props;
+    return <div className="productLine">
+        <div>{product.name}</div>
+        <div>{product.price} euro</div>
+    </div>
 }
 
 function App() {
   return (
       <div>
         <h1>Menu</h1>
-        <ProductLine productName = "Cola" productCost = "1€"/>
-        <ProductLine productName = "Water" productCost = "1€"/>
-        <ProductLine productName = "Bier" productCost = "1€"/>
-        <ProductLine productName = "Wijn" productCost = "2€"/>
+            <ProductLine product = {PRODUCTS_DATA[0]}/>
+            <ProductLine product = {PRODUCTS_DATA[1]}/>
+            <ProductLine product = {PRODUCTS_DATA[2]}/>
+            <ProductLine product = {PRODUCTS_DATA[3]}/>
+            <ProductLine product = {PRODUCTS_DATA[4]}/>
       </div>
   );
 }
